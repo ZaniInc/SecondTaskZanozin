17030ce623da8f7a747ee8f185ffc5bab0902ed4
ZaniInc <stridestoreholding@gmail.com>
2022-06-23 11:03:10 +0300

    "test"

--- migrations/2_deploy_contracts.js
+++
@@ -1,9 +0,0 @@
-var MyToken = artifacts.require("./MyToken.sol");
-var MyTokenn = artifacts.require("./MyTokenn.sol");
-var AirDrop = artifacts.require("./AirDrop.sol");
-
-module.exports = async (deployer) => {
-  await deployer.deploy(MyToken);
-  await deployer.deploy(AirDrop, MyToken.address);
-  await deployer.deploy(MyTokenn);
-};
