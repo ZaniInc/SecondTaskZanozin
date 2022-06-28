c488cd05b4448794b0fa0eacd106d3ba9a399577
ZaniInc <stridestoreholding@gmail.com>
2022-06-28 20:58:23 +0300

    "test3"

--- truffle-config.js
+++ truffle-config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const HDWalletProvider = require("@truffle/hdwallet-provider");
-// require("dotenv").config({path: "./.env"});
-// const AccountIndex = 0;
+require("dotenv").config({path: "./.env"});
+const AccountIndex = 0;
 
 module.exports = {
   networks: {
@@ -10,26 +10,26 @@ module.exports = {
       host: "127.0.0.1",
       network_id: "*"
     }
-    // goerli_infura : {
-    //   provider : function () {
-    //     return new HDWalletProvider(process.env.MNEMONIC,"https://goerli.infura.io/v3/23b0f66c3c2b4d77ad961d2f97c04e44", AccountIndex);
-    //   },
-    //   network_id: 5
-    // },
-    // ropsten_infura : {
-    //   provider : function () {
-    //     return new HDWalletProvider(process.env.MNEMONIC,"wss://ropsten.infura.io/ws/v3/23b0f66c3c2b4d77ad961d2f97c04e44",AccountIndex);
-    //   },
-    //   network_id: 3
-    // }
+    goerli_infura : {
+      provider : function () {
+        return new HDWalletProvider(process.env.MNEMONIC,"https://goerli.infura.io/v3/23b0f66c3c2b4d77ad961d2f97c04e44", AccountIndex);
+      },
+      network_id: 5
+    },
+    ropsten_infura : {
+      provider : function () {
+        return new HDWalletProvider(process.env.MNEMONIC,"wss://ropsten.infura.io/ws/v3/23b0f66c3c2b4d77ad961d2f97c04e44",AccountIndex);
+      },
+      network_id: 3
+    }
   },
   compilers: {
     solc: {
       version: "0.8.7"
     }
   }
-  // plugins:['truffle-plugin-verify'] ,
-  // api_keys :{
-  //   etherscan: 'HNYS8RTW749XT7W8VCZRH8SDV4YF2X7Y8T'
-  // }
+  plugins:['truffle-plugin-verify'] ,
+  api_keys :{
+    etherscan: 'HNYS8RTW749XT7W8VCZRH8SDV4YF2X7Y8T'
+  }
 };
